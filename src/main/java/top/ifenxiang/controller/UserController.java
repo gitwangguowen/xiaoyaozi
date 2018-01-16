654ee57e72c9338b588dab69da1f196e9e1866e7
gitwangguowen <java_wangguowen@foxmail.com>
2018-01-16 17:41:49 +0800

    commit

---
+++ src/main/java/top/ifenxiang/controller/UserController.java
@@ -0,0 +1,30 @@
+package top.ifenxiang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import top.ifenxiang.pojo.User;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/user/getUser/{id}")
+	@ResponseBody
+	public User getUser(@PathVariable Long id,String username) {
+		System.out.println(id);
+		User user = new User();
+		user.setName("tom");
+		user.setAge("20");
+		user.setSex("男");
+		return user;
+	}
+	
+	@RequestMapping("/user/setUser")
+	@ResponseBody
+	public User setUser(User user) {
+		return user;
+	}
+	
+}
